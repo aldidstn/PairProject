9ea45c0680feceba52fa01fe7e556e1889506a2b
Your Name <you@example.com>
2020-11-20 03:34:18 +0700

    reg login hash psd

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const router = require('./routes')
+const session = require('express-session');
 const port = 5000
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
+
+app.use(session({
+  secret: 'login itu rahasia',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    sameSite: true 
+  }
+}))
+app.use((req,res, next) => {
+  if(!req.session.userId){
+    const error = 'Please Login first!!!!'
+    res.send(error)
+  }
+  else{
+    next()
+  }
+})
 
 app.use('/', router);
 
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,8 +1,29 @@
 const router = require('express').Router()
+const session = require('express-session');
 const UserController = require('../controllers/UserController')
 
-router.get('/', UserController.loginForm)
-router.post('/', UserController.postLogin)
 
+router.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      sameSite: true 
+    }
+  }))
+
+router.use((req,res, next) => {
+    if(!req.session.userId){
+      const error = 'Please Login first!!!!'
+      res.send(error)
+    }
+    else{
+      next()
+    }
+  })
+
+  router.get('/', UserController.loginForm)
+router.post('/', UserController.postLogin)
 
 module.exports = router
